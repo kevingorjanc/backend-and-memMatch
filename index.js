09628be9bcbd2566389a4c9e28f4cacab39b2260
kevin gorjanc <kevin@tutorolearning.com>
2020-03-10 20:26:02 -0700

    x

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 
+const port = process.env.PORT || 5000;
 
 //configure body-parser for express
 app.use(bodyParser.urlencoded({extended:false}));
@@ -45,11 +46,11 @@ app.route('/high-scores')
                     });
     });
 
-app.route('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
+// app.route('*', (req, res) => {
+//   res.sendFile(path.join(__dirname+'/client/build/index.html'));
+// });
 
-const port = process.env.PORT || 5000;
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
