ab396c616c0094f3631bfadae6fe05b8e5ee9392
kevin gorjanc <kevin@tutorolearning.com>
2020-03-11 18:31:26 -0700

    add child commits

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ app.use(bodyParser.json());
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-// app.route('/favicon.ico')
-//     .get( (req, res) => {
-//         console.log(req.body);
-//         res.send("this is from back");
-//     });
+app.route('/favicon.ico')
+    .get( (req, res) => {
+        console.log(req.body);
+        res.send("this is from back");
+    });
 app.route('/high-scores')
     .get( (request, response) => {
         console.log("responding");
